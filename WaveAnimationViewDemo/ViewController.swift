fa195dcc680ccbc206c48934004227a8eea96a09
obaya884 <taku3.884@gmail.com>
2020-07-01 15:42:27 +0900

    Initial Commit

---
+++ WaveAnimationViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaveAnimationViewDemo
+//
+//  Created by 大林拓実 on 2020/07/01.
+//  Copyright © 2020 Obayashi Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
